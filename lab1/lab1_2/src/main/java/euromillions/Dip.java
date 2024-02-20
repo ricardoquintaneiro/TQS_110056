3f6c0f99a0c9db0018f2abd17bf438d6884bb3d8
Ricardo Quintaneiro <97852246+ricardoquintaneiro@users.noreply.github.com>
2024-02-20 15:48:52 +0000

    Start of Lab1_2

---
+++ lab1/lab1_2/src/main/java/euromillions/Dip.java
@@ -0,0 +1,17 @@
+package euromillions;
+
+public class Dip {
+    private final int STARS_RANGE_MAX;
+    private final BoundedSetOfNaturals numbers;
+    private final int STARS_REQUIRED;
+    private final int NUMBERS_REQUIRED;
+    private final BoundedSetOfNaturals stars;
+    private final int NUMBERS_RANGE_MAX;
+    private final RANDOM generator;
+
+    public Dip(int[] one, int[] two) {
+    }
+
+    public Dip() {
+    }
+}
