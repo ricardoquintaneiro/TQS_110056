98e831fde81589e8ff3933d5991a2c7687db1473
Ricardo Quintaneiro <97852246+ricardoquintaneiro@users.noreply.github.com>
2024-02-22 00:57:57 +0000

    Lab1_2 completed

--- lab1/lab1_2/src/test/java/tqs/sets/BoundedSetOfNaturalsTest.java
+++ lab1/lab1_2/src/test/java/tqs/sets/BoundedSetOfNaturalsTest.java
@@ -38,12 +38,7 @@ class BoundedSetOfNaturalsTest {
         Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
             setB.add(11);
         });
-        assertEquals(exception.getMessage(), "bounded set is full. no more elements allowed.");
-
-        exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            setB.add(50);
-        });
-        assertEquals(exception.getMessage(), "duplicate value: 50");
+        assertEquals("bounded set is full. no more elements allowed.", exception.getMessage());
     }
 
     @Test
@@ -58,4 +53,37 @@ class BoundedSetOfNaturalsTest {
         assertEquals("Illegal argument: not a natural number", exception.getMessage());
     }
 
+    @Test
+    public void testIntersects() {
+        assertTrue(setB.intersects(setC), "intersects: sets do intersect");
+        assertFalse(setA.intersects(setB), "intersects: sets do not intersect");
+        setA.add(1);
+        assertFalse(setA.intersects(setB), "intersects: sets do not intersect after adding 1");
+    }
+
+    @Test
+    public void testAddMultiples() {
+        BoundedSetOfNaturals newSet = new BoundedSetOfNaturals(5);
+        newSet.add(new int[] { 1, 2, 3, 4, 5 });
+        assertTrue(newSet.contains(5), "add: added element not found in set.");
+        assertEquals(5, newSet.size());
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            newSet.add(new int[] { 6, 7, 8, 9, 10, 11 });
+        });
+        assertEquals("bounded set is full. no more elements allowed.", exception.getMessage());
+    }
+
+    @Test
+    public void testAddDuplicates() {
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            setB.add(50);
+        });
+        assertEquals(exception.getMessage(), "duplicate value: 50");
+
+        exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            setA.add(new int[] { 50, 50 });
+        });
+        assertEquals(exception.getMessage(), "duplicate value: 50");
+    }
 }
