0e11131d19ae5c79951d282d1fdafb44ca06a44e
Ricardo Quintaneiro <97852246+ricardoquintaneiro@users.noreply.github.com>
2024-02-29 11:48:17 +0000

    Lab2_2 tests

--- lab2/lab2_2geocoding/src/test/java/geocoding/AddressResolverTest.java
+++ lab2/lab2_2geocoding/src/test/java/geocoding/AddressResolverTest.java
@@ -1,36 +1,98 @@
 package geocoding;
 
-import connection.ISimpleHttpClient;
-import connection.TqsBasicHttpClient;
-import org.json.simple.parser.ParseException;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyString;
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import connection.TqsBasicHttpClient;
 
 @ExtendWith(MockitoExtension.class)
 class AddressResolverTest {
 
+    @Mock
+    TqsBasicHttpClient httpClient;
+
+    @InjectMocks
     AddressResolverService resolver;
 
     @Test
-    @Disabled
+    @DisplayName("When a valid location is provided, then it should return the correct address")
     void whenResolveDetiGps_returnJacintoMagalhaeAddress() throws ParseException, IOException, URISyntaxException {
 
-        //todo: implement test; remove Disabled annotation
+        String response = "{\n" +
+                "  \"info\": {\n" +
+                "    \"statuscode\": 0,\n" +
+                "    \"copyright\": {\n" +
+                "      \"text\": \"© 2024 MapQuest, Inc.\",\n" +
+                "      \"imageUrl\": \"http://api.mqcdn.com/res/mqlogo.gif\",\n" +
+                "      \"imageAltText\": \"© 2024 MapQuest, Inc.\"\n" +
+                "    },\n" +
+                "    \"messages\": []\n" +
+                "  },\n" +
+                "  \"options\": {\n" +
+                "    \"maxResults\": 1,\n" +
+                "    \"ignoreLatLngInput\": false\n" +
+                "  },\n" +
+                "  \"results\": [\n" +
+                "    {\n" +
+                "      \"providedLocation\": {\n" +
+                "        \"latLng\": {\n" +
+                "          \"lat\": 40.63436,\n" +
+                "          \"lng\": -8.65616\n" +
+                "        }\n" +
+                "      },\n" +
+                "      \"locations\": [\n" +
+                "        {\n" +
+                "          \"street\": \"Avenida da Universidade\",\n" +
+                "          \"adminArea6\": \"Aveiro\",\n" +
+                "          \"adminArea6Type\": \"Neighborhood\",\n" +
+                "          \"adminArea5\": \"Aveiro\",\n" +
+                "          \"adminArea5Type\": \"City\",\n" +
+                "          \"adminArea4\": \"Aveiro\",\n" +
+                "          \"adminArea4Type\": \"County\",\n" +
+                "          \"adminArea3\": \"\",\n" +
+                "          \"adminArea3Type\": \"State\",\n" +
+                "          \"adminArea1\": \"PT\",\n" +
+                "          \"adminArea1Type\": \"Country\",\n" +
+                "          \"postalCode\": \"3810-489\",\n" +
+                "          \"geocodeQualityCode\": \"B1AAA\",\n" +
+                "          \"geocodeQuality\": \"STREET\",\n" +
+                "          \"dragPoint\": false,\n" +
+                "          \"sideOfStreet\": \"L\",\n" +
+                "          \"linkId\": \"0\",\n" +
+                "          \"unknownInput\": \"\",\n" +
+                "          \"type\": \"s\",\n" +
+                "          \"latLng\": {\n" +
+                "            \"lat\": 40.63437,\n" +
+                "            \"lng\": -8.65625\n" +
+                "          },\n" +
+                "          \"displayLatLng\": {\n" +
+                "            \"lat\": 40.63437,\n" +
+                "            \"lng\": -8.65625\n" +
+                "          },\n" +
+                "          \"mapUrl\": \"\"\n" +
+                "        }\n" +
+                "      ]\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
 
-        // will crash for now...need to set the resolver before using it
+        when(httpClient.doHttpGet(anyString())).thenReturn(response);
+
         Optional<Address> result = resolver.findAddressForLocation(40.63436, -8.65616);
 
         //return
@@ -42,11 +104,34 @@ class AddressResolverTest {
     }
 
     @Test
-    @Disabled
+    @DisplayName("When invalid coordinates are provided, then it should return no valid address")
     public void whenBadCoordidates_thenReturnNoValidAddress() throws IOException, URISyntaxException, ParseException {
 
-        ///todo: implement test
+        String invalidResponse = "{\n" +
+        "  \"info\": {\n" +
+        "    \"statuscode\": 400,\n" +
+        "    \"copyright\": {\n" +
+        "      \"text\": \"© 2024 MapQuest, Inc.\",\n" +
+        "      \"imageUrl\": \"http://api.mqcdn.com/res/mqlogo.gif\",\n" +
+        "      \"imageAltText\": \"© 2024 MapQuest, Inc.\"\n" +
+        "    },\n" +
+        "    \"messages\": [\n" +
+        "      \"Illegal argument from request: Invalid LatLng specified.\"\n" +
+        "    ]\n" +
+        "  },\n" +
+        "  \"options\": {\n" +
+        "    \"maxResults\": 1,\n" +
+        "    \"ignoreLatLngInput\": false\n" +
+        "  },\n" +
+        "  \"results\": [\n" +
+        "    {\n" +
+        "      \"providedLocation\": {},\n" +
+        "      \"locations\": []\n" +
+        "    }\n" +
+        "  ]\n" +
+        "}";
 
+        when(httpClient.doHttpGet(anyString())).thenReturn(invalidResponse);
 
         Optional<Address> result = resolver.findAddressForLocation( -361,-361);
         // verify no valid result
