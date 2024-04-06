0535c7cd2ea47e05362e9714446a47164258eb51
Ricardo Quintaneiro <97852246+ricardoquintaneiro@users.noreply.github.com>
2024-04-06 01:03:28 +0100

    Hw1 start files

---
+++ hw1/bus-ticket-backend/src/test/java/pt/ua/tqs110056/busticketbackend/BusTicketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.ua.tqs110056.busticketbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTicketBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ hw1/bus-ticket-backend/src/test/java/pt/ua/tqs110056/busticketbackend/TestBusTicketBackendApplication.java
@@ -0,0 +1,23 @@
+package pt.ua.tqs110056.busticketbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestBusTicketBackendApplication {
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(BusTicketBackendApplication::main).with(TestBusTicketBackendApplication.class).run(args);
+	}
+
+}
