267e1bbf1ad0d2a0bc4aa5ccb0e308636e4138d6
Ricardo Quintaneiro <97852246+ricardoquintaneiro@users.noreply.github.com>
2024-04-09 13:40:31 +0100

    Hw1 frontend select menu

--- hw1/bus-ticket-frontend/src/routes/home.tsx
+++ hw1/bus-ticket-frontend/src/routes/home.tsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom"
+import { SelectMenu } from "../components/selectmenu"
 
 export const HomePage = () => {
   return (
-    <>
-      <div>
-        <Link to={"/trips"}>Trips</Link>
-      </div>
-    </>
+    <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-xl">
+      <h1 className="text-xl font-semibold mb-4">Choose your departure city:</h1>
+      <SelectMenu />
+      <h1 className="text-xl font-semibold mb-4">Choose your destination city:</h1>
+      <SelectMenu />
+      <Link to="/trips" className="block text-center py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600">
+        Trips
+      </Link>
+    </div>
+
   )
 }
